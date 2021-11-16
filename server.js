855c96646d6411b0886e7d88beb30cc4b3811234
swapnil9595 <65943650+swapnil9595@users.noreply.github.com>
2021-11-16 11:19:17 -0500

    heroku server

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.get("/hello", (req, res) => {
 
 require("./services/movies-service")(app);
 require("./services/tweeter-service")(app);
-//require("./services/profile-service")(app);
+require("./services/profile-service")(app);
 
 app.listen(4000);
--- services/profile-service.js
+++ services/profile-service.js
@@ -1,5 +1,24 @@
 let profile = require("../data/profile.json");
 
 module.exports = (app) => {
-  const getCurrentProfile = (req, res) => r;
+  const getCurrentProfile = (req, res) => {
+    res.json(profile);
+  };
+
+  const updateCurrentProfile = (req, res) => {
+    profile = {
+      _id: new Date().getTime() + "",
+      profilePicture: "../../images/dp.jpg",
+      bannerPicture: "../../images/matrix.jpg",
+      ...req.profile,
+      dateJoined: "September 20",
+      followingCount: 312,
+      followersCount: 180,
+    };
+    res.sendStatus(200);
+    //res.json(updatedProfile);
+  };
+
+  app.get("/api/profile", getCurrentProfile);
+  app.put("/api/profile", updateCurrentProfile);
 };
